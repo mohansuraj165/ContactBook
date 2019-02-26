914fc0adf5dd21edf6e2551c84527465e55dd8aa
Mohan Suraj <mohansuraj165@gmail.com>
2019-02-25 18:05:38 -0700

    adding background

--- src/App.js
+++ src/App.js
@@ -7,27 +7,28 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Create from './components/create.component';
 import Edit from './components/edit.component';
 import Index from './components/index.component';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
 
-        <div className="container">
+        <div className="container App">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
-            <i class="fas fa-address-card"></i>
-            <Link to={'/'} className="navbar-brand">Contactem</Link>
+            <div className="navbar-brand">
+            <i  class="fas fa-address-card"></i>
+            </div>
+            <Link to={'/create'} className="navbar-brand">Contactem</Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                  <Link to={'/'} className="nav-link">Home</Link>
-                </li>
+              
                 <li className="nav-item">
                   <Link to={'/create'} className="nav-link">Create</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to={'/index'} className="nav-link">Index</Link>
+                  <Link to={'/index'} className="nav-link">View List</Link>
                 </li>
               </ul>
             </div>
